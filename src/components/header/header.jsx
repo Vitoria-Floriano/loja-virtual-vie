1edd50a6bb7657b711d65a9a415904e0c37d8616
Vitoria Floriano <viefloriano@hotmail.com>
2024-09-29 23:37:02 -0300

    importando imagem logo

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,12 @@
 import "./header.css"
 import { Link } from "react-router-dom"
+import Logotipo from "../../images/logotipo-removebg-preview.png"
 
 export const Menu = () => {
     return (
         <header className="menu">
             <div>
-                <Link to="/"><img src="./src/images/logotipo-removebg-preview.png" className="logo" alt="logo da loja" /></Link>
+                <Link to="/"><img src={Logotipo} className="logo" alt="logo da loja" /></Link>
             </div>
             <div>
                 <Link className="button" to="/about">Sobre</Link>
